14976fd4e9a0c9323028561c07b839b21857e019
Shalom91 <you@example.com>
2021-03-25 11:55:19 +0200

    first commit

---
+++ Python.py
@@ -0,0 +1,29 @@
+class Animal:
+
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("Rax eats!")
+
+    def bark(self):
+        print("Woof!")
+
+my_dog = Animal("Rax", "SuperDog")
+
+my_dog.bark()
+
+
+
+ class Cat extends Animal():
+
+   self.makeSound()
+ 
+     System.out.println("meow");
+
+
+
+ class Dog extends Animal():
+
+   makeSound()
+
+       System.out.println("woof");
